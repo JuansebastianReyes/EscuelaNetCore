f9736d649193861eb5c2658a0345def8f9bf05ce
SEBASTIAN REYES <reyes_garcia1103@hotmail.com>
2021-06-08 17:23:09 -0500

    Fin etapa 6

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -16,7 +16,7 @@ namespace CoreEscuela
 
         public void Inicializar()
         {
-            Escuela = new Escuela("Platzi Academay", 2012, TiposEscuela.Primaria,
+            Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria,
             ciudad: "Bogotá", pais: "Colombia"
             );
 
@@ -26,6 +26,25 @@ namespace CoreEscuela
 
         }
 
+        public List<ObjetoEscuelaBase> GetObjetosEscuela()
+        {
+            var listaObj = new List<ObjetoEscuelaBase>();
+            listaObj.Add(Escuela);
+            listaObj.AddRange(Escuela.Cursos);
+            foreach (var curso in Escuela.Cursos)
+            {
+                listaObj.AddRange(curso.Asignaturas);
+                listaObj.AddRange(curso.Alumnos);
+
+                foreach (var alumno in curso.Alumnos)
+                {
+                    listaObj.AddRange(alumno.Evaluaciones);
+                }
+            }
+            return listaObj;
+        }
+        
+        #region Metodos De Carga
         private void CargarEvaluaciones()
         {
             var lista = new List<Evaluacion>();
@@ -51,24 +70,6 @@ namespace CoreEscuela
                     }
                 }
             }
-        }
-
-        public List<ObjetoEscuelaBase> GetObjetosEscuela()
-        {
-            var listaObj = new List<ObjetoEscuelaBase>();
-            listaObj.Add(Escuela);
-            listaObj.AddRange(Escuela.Cursos);
-            foreach (var curso in Escuela.Cursos)
-            {
-                listaObj.AddRange(curso.Asignaturas);
-                listaObj.AddRange(curso.Alumnos);
-
-                foreach (var alumno in curso.Alumnos)
-                {
-                    listaObj.AddRange(alumno.Evaluaciones);
-                }
-            }
-            return listaObj;
         }
 
         private void CargarAsignaturas()
@@ -116,5 +117,6 @@ namespace CoreEscuela
                 c.Alumnos = GenerarAlumnosAlAzar(cantRandom);
             }
         }
+        #endregion
     }
 }
--- Entidades/ObjetoEscuelaBase.cs
+++ Entidades/ObjetoEscuelaBase.cs
@@ -3,7 +3,7 @@ using System;
 namespace CoreEscuela.Entidades
 
 {
-    public /*abstract*/ class ObjetoEscuelaBase
+    public abstract class ObjetoEscuelaBase
     {
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
